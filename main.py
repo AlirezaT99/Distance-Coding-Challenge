9ba732f7805d83da9b3fb23c4fbd6be31c923925
Alireza Tajmirriahi <alireza.tajmirriahi@aalto.fi>
2024-12-04 22:16:50 +0200

    Debug and document the code

--- main.py
+++ main.py
@@ -3,10 +3,13 @@
 # GitHub: https://github.com/AlirezaT99/Distance-Coding-Challenge
 
 import os
+import re
 import argparse
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+
+tqdm.pandas()  # Enables progress_apply
 
 from Bio import SeqIO  # For reading FASTA files
 import mmh3  # For Murmur hashing
@@ -27,36 +30,46 @@ output = ""
 
 
 def dprint(msg) -> None:
+    global output
     output += msg + "\n"
     if DEBUG:
         print(msg)
 
-def get_kmers(sequences: list, kmer_size: int) -> pd.Series:
+
+def extract_kmers(sequences: list, kmer_size: int) -> pd.Series:
     """
-    Returns a list of all kmers of size kmer_size in the given sequence.
+    Returns a list of all valid kmers of size kmer_size in
+    the given sequence, after removing ambiguous base chunks.
     """
+    sequences = filter(
+        lambda fragment: len(fragment) >= kmer_size,
+        [fragment for sequence in sequences for fragment in re.split("N+", str(sequence.seq))]
+    )  # Naive way of cutting out ambiguous base chunks
     result = []
     for sequence in sequences:
-        result.extend([sequence.seq[i:i + kmer_size] for i in tqdm(range(len(sequence) - kmer_size + 1))])
+        result.extend([sequence[i:i + kmer_size] for i in tqdm(range(len(sequence) - kmer_size + 1))])
     return pd.Series(result)
+
 
 def calculate_hash(kmers: pd.Series) -> np.array:
     """
     Returns the Murmur hash of the given kmers by assigning the minimum value
     between the kmer and its reverse complement as the canonical kmer hash.
     """
+
     def reverse_complement(sequence: str) -> str:
         """
         Returns the reverse complement of the given sequence.
         """
         return "".join(COMPLEMENTS[base] for base in sequence[::-1])
-    
+
     def hash_kmer(kmer: str) -> int:
         return mmh3.hash(kmer, seed=0, signed=False)
 
-    rev_kmers = kmers.apply(reverse_complement)
-    kmers_hash, rev_kmers_hash = kmers.apply(hash_kmer), rev_kmers.apply(hash_kmer)
+    rev_kmers = kmers.progress_apply(reverse_complement)
+    kmers_hash, rev_kmers_hash = kmers.progress_apply(hash_kmer), rev_kmers.progress_apply(hash_kmer)
     return np.min([kmers_hash, rev_kmers_hash], axis=0)
+
 
 def test_hash_consistency(sequences: pd.Series, hash_values, attempts=1) -> None:
     """
@@ -66,13 +79,16 @@ def test_hash_consistency(sequences: pd.Series, hash_values, attempts=1) -> None
     for _ in range(attempts):
         attempt_values = calculate_hash(sequences)
         assert (hash_values == attempt_values).all(), "Hash values are inconsistent."
+    dprint("\tHash values are consistent.")
 
-def make_sketch(kmers: set, sketch_size: int) -> np.array:
+
+def make_sketch(kmers: pd.Series, sketch_size: int) -> np.array:
     """
     Returns a MinHash sketch of the given kmers with the given sketch size.
     """
-    hash_values = calculate_hash(kmers)
+    hash_values = calculate_hash(kmers.drop_duplicates())
     return np.sort(hash_values)[:sketch_size]
+
 
 def jaccard_distance(kmers1, kmers2) -> float:
     """
@@ -81,59 +97,76 @@ def jaccard_distance(kmers1, kmers2) -> float:
     """
     return 1 - len(set(kmers1) & set(kmers2)) / len(set(kmers1) | set(kmers2))
 
+
 def read_fasta(file_path) -> list:
     """
     Reads the FASTA file at the given path and returns a SeqRecord iterator.
     """
-    with open(file_path, "r") as file:
-        return list(SeqIO.parse(file, "fasta"))
+    with open(file_path, "r") as f:
+        return list(SeqIO.parse(f, "fasta"))
 
-def main(args) -> None:    
-    file_pair = (0, 1) if args.seq_pair == "references" else (2, 3)
-    
-    dprint("Step 1 - Read FASTA files and calculate kmer counts")
-    files_kmer_counts = dict()  # Storing the kmer counts dict for each file
-    file_names = FILE_PATHS[file_pair[0]], FILE_PATHS[file_pair[1]]
+
+def main(args) -> None:
+    dprint("Step 1 - Reading FASTA files and calculating kmer counts")
+    kmer_counts = dict()  # Storing the kmer counts dict for each file
+    kmers_dict = dict()
     for file_path in FILE_PATHS:
+        dprint(f"\tReading {file_path}...")
         sequences = read_fasta(f"{args.data_dir}/{file_path}")
-        kmers = get_kmers(sequences, args.kmer_size)
+
+        dprint(f"\tExtracting kmers from {file_path}...")
+        kmers = extract_kmers(sequences, args.kmer_size)
         kmers_count = kmers.value_counts().to_dict()
-        files_kmer_counts[file_path] = kmers_count
-    
-    # Step 2 - Calculate Jaccard distance between the two files
-    distance = jaccard_distance(files_kmer_counts[file_names[0]].keys(), files_kmer_counts[file_names[1]].keys())
-    output = f"Jaccard distance between {file_names[0]} and {file_names[1]}: {distance}"
-    
-    # Step 3 - Calculate MinHash signatures
+        kmers_dict[file_path] = kmers
+        kmer_counts[file_path] = kmers_count
+
+    dprint("Step 2 - Calculating Jaccard distance between the two pairs")
+    for pair in [(0, 1), (2, 3)]:  # Indices in the paths list
+        distance = jaccard_distance(kmer_counts[FILE_PATHS[pair[0]]].keys(), kmer_counts[FILE_PATHS[pair[1]]].keys())
+        dprint(f"\tJaccard distance between {FILE_PATHS[pair[0]]} and {FILE_PATHS[pair[1]]}: {distance}\n")
+
+    dprint("Step 3 - Calculating MinHash signatures")
     for file_path in FILE_PATHS:
-        hash_values = calculate_hash(kmers)
-        test_hash_consistency(kmers, hash_values)
-    
-    # Step 4 - Create a Sketch from input sequences
+        dprint(f"\tCalculating Hash for {file_path} kmers (three steps)...")
+        hash_values = calculate_hash(kmers_dict[file_path])
+        dprint(f"\tTesting Hash consistency...")
+        test_hash_consistency(kmers_dict[file_path], hash_values)
+
+    dprint("Step 4 - Creating a Sketch from input sequences")
     for file_path in FILE_PATHS:
-        sketch = make_sketch(files_kmer_counts[file_path].keys(), args.sketch_size)
-        np.save(f"{args.output_dir}/{file_path}_sketch_{args.sketch_size}.npy", sketch)
-    
-    # Step 5 - Calculate Jaccard distance between the two sketches
-    for pair in [(0, 1), (2, 3)]:
+        dprint(f"\tMaking sketch for {file_path} with size {args.sketch_size}")
+        sketch = make_sketch(kmers_dict[file_path], args.sketch_size)
+        output_sketch_path = f"{args.output_dir}/{file_path}_sketch_{args.sketch_size}.npy"
+        np.save(output_sketch_path, sketch)
+        dprint(f"\tSaved sketch to {output_sketch_path}.")
+
+    dprint("Step 5 - Calculating Jaccard distance between the two sketches")
+    for pair in [(0, 1), (2, 3)]:  # Indices in the paths list
         sketch1 = np.load(f"{args.output_dir}/{FILE_PATHS[pair[0]]}_sketch_{args.sketch_size}.npy")
         sketch2 = np.load(f"{args.output_dir}/{FILE_PATHS[pair[1]]}_sketch_{args.sketch_size}.npy")
         distance = jaccard_distance(sketch1, sketch2)
-        dprint(f"\nJaccard distance between {FILE_PATHS[pair[0]]} and {FILE_PATHS[pair[1]]} sketches: {distance}")
-    # TODO Comparing Isolates and Neighbor Joining
-    
+        dprint(f"\tJaccard distance between {FILE_PATHS[pair[0]]} and {FILE_PATHS[pair[1]]} sketches: {distance}")
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Distance Coding Challenge - Alireza Tajmirriahi")
-    parser.add_argument("--seq-pair", "-p", type=str, help="Sequence pairs to analyze", choices=["references", "draft"], default="references")
     parser.add_argument("--data-dir", "-d", type=str, help="Data directory path", default="s_pneumoniae_genomes")
     parser.add_argument("--kmer-size", "-k", type=int, help="K-mer size", default=DEFAULT_KMER_SIZE)
     parser.add_argument("--sketch-size", "-s", type=int, help="Sketch size", default=DEFAULT_SKETCH_SIZE)
     parser.add_argument("--output-dir", "-o", type=str, help="Output directory path", default="output")
     return parser.parse_args()
 
+
 if __name__ == "__main__":
     args = parse_args()
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
-    main(args)
+    try:
+        main(args)
+    except Exception as e:
+        dprint(f"An error occurred: {e}")
+    finally:
+        output_log_path = os.path.join(args.output_dir, f"log-{pd.Timestamp.now().strftime('%H-%M-%S')}.txt")
+        with open(output_log_path, "w") as file:
+            file.write(output)
+        print(f"Log written to {output_log_path}.")
